e80adbc1c12affbbd0f0da4e4b76a24d0e4b5eca
Grzegorz Świrski <grzegorz@swirski.name>
2014-12-26 14:27:08 +0100

    move player

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,29 @@ use std::ptr;
 use std::str;
 use gl::types::*;
 use glutin::{
+    ElementState,
     Event,
     VirtualKeyCode
 };
+
+struct Player {
+    x: f32,
+    y: f32,
+}
+
+impl Player {
+    fn move_p(&mut self, dir: int) {
+        self.x += 0.01 * dir as f32;
+    }
+
+    fn to_vertex_data(&self) -> Vec<GLfloat> {
+        return vec![
+            self.x, self.y + 0.4,
+            self.x - 0.1, self.y,
+            self.x + 0.1, self.y,
+        ];
+    }
+}
 
 static VERTEX_DATA: [GLfloat, ..12] = [
     -0.5, -0.4,
@@ -90,6 +110,13 @@ fn main() {
 
     gl::load_with(|s| window.get_proc_address(s));
 
+    let mut player_a = Player { x: -0.5, y: -0.8 };
+    let player_b = Player { x: 0.5, y: -0.8 };
+
+    let mut vertex_data: Vec<GLfloat> = vec![];
+    vertex_data.push_all(player_a.to_vertex_data().as_slice());
+    vertex_data.push_all(player_b.to_vertex_data().as_slice());
+
     let mut vao = 0;
     let mut vbo = 0;
 
@@ -101,7 +128,7 @@ fn main() {
         gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
         gl::BufferData(gl::ARRAY_BUFFER,
                        (VERTEX_DATA.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
-                       mem::transmute(&VERTEX_DATA[0]),
+                       mem::transmute(&vertex_data[0]),
                        gl::DYNAMIC_DRAW);
 
         let vs = compile_shader(VS_SRC, gl::VERTEX_SHADER);
@@ -115,15 +142,32 @@ fn main() {
         gl::EnableVertexAttribArray(pos_attr as GLuint);
     }
 
+    let mut right = 0;
+    let mut left = 0;
+
     'main: loop {
         for event in window.poll_events() {
             match event {
+                Event::KeyboardInput(ElementState::Pressed, _, Some(VirtualKeyCode::Right)) => right = 1,
+                Event::KeyboardInput(ElementState::Released, _, Some(VirtualKeyCode::Right)) => right = 0,
+                Event::KeyboardInput(ElementState::Pressed, _, Some(VirtualKeyCode::Left)) => left = 1,
+                Event::KeyboardInput(ElementState::Released, _, Some(VirtualKeyCode::Left)) => left = 0,
                 Event::KeyboardInput(_, _, Some(VirtualKeyCode::Escape)) => break 'main,
                 _ => ()
             }
         }
 
+        player_a.move_p(right - left);
+
         unsafe {
+            vertex_data.clear();
+            vertex_data.push_all(player_a.to_vertex_data().as_slice());
+            vertex_data.push_all(player_b.to_vertex_data().as_slice());
+            gl::BufferData(gl::ARRAY_BUFFER,
+                           (VERTEX_DATA.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+                           mem::transmute(&vertex_data[0]),
+                           gl::DYNAMIC_DRAW);
+
             gl::ClearColor(0.3, 0.3, 0.3, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
 
