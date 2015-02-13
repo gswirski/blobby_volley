34ea06dc1bb406bbe6153d2bccf982e5d3a5cb35
Grzegorz Świrski <grzegorz@swirski.name>
2015-02-13 19:02:00 +0100

    geom primitives

--- src/geom.rs
+++ src/geom.rs
@@ -1,10 +1,12 @@
 use std::num::Float;
 
+#[derive(Clone)]
 pub struct Vec2 {
     pub x: f32,
     pub y: f32,
 }
 
+#[derive(Clone)]
 pub struct Point {
     pub x: f32,
     pub y: f32,
@@ -18,14 +20,15 @@ pub struct Line {
 pub struct Circle {
     pub center: Point,
     pub radius: f32,
+    pub velocity: Vec2,
 }
 
 impl Vec2 {
-    pub fn vector_between(a: &Point, b: &Point) -> Vec2 {
-        Vector { x: b.x - a.x, y: b.y - a.y }
+    pub fn between(a: &Point, b: &Point) -> Vec2 {
+        Vec2 { x: b.x - a.x, y: b.y - a.y }
     }
 
-    pub fn magnitude(&self) {
+    pub fn magnitude(&self) -> f32 {
         (self.x * self.x + self.y * self.y).sqrt()
     }
 
@@ -37,7 +40,11 @@ impl Vec2 {
 
 impl Line {
     pub fn to_unit_vector(&self) -> Vec2 {
-        vector_between(self.start, self.end).to_unit()
+        Vec2::between(&self.start, &self.end).to_unit()
+    }
+
+    pub fn len(&self) -> f32 {
+        distance(&self.start, &self.end)
     }
 }
 
@@ -53,6 +60,33 @@ pub fn distance(a: &Point, b: &Point) -> f32 {
 
 pub fn closest_point(circle: &Circle, line: &Line) -> Point {
     let line_vec = line.to_unit_vector();
-    let line_end_to_circle = vector_between(line.start, circle.center);
-    let dot = dot_product(line_end_to_circle, line_vec);
+    let line_end_to_circle = Vec2::between(&line.start, &circle.center);
+    let dot = dot_product(&line_end_to_circle, &line_vec);
+
+    if dot <= 0.0 {
+        line.start.clone()
+    } else if dot >= line.len() {
+        line.end.clone()
+    } else {
+        Point {
+            x: line.start.x + line_vec.x * dot,
+            y: line.start.y + line_vec.y * dot,
+        }
+    }
 }
+
+impl Circle {
+    fn bounce_vector(&self, line: &Line) -> Vec2 {
+        // @todo: make it long enough to cover radius as well
+        Vec2::between(&closest_point(self, line), &self.center)
+    }
+
+    pub fn bounce_circle(&mut self, line: &Line) {
+        let bounce = self.bounce_vector(line);
+        let bounce_normal = bounce.to_unit();
+        let dot = dot_product(&self.velocity, &bounce_normal);
+        self.velocity.x -= 2.0 * dot * bounce_normal.x;
+        self.velocity.y -= 2.0 * dot * bounce_normal.y;
+    }
+}
+
